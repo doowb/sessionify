e1f90704be4a323f75b28acac47d5d27a9dd0c26
Brian Woodward <brian.woodward@gmail.com>
2014-12-16 21:51:27 -0500

    setup

---
+++ index.js
@@ -0,0 +1,38 @@
+/*!
+ * sessionify <https://github.com/doowb/sessionify>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = sessionify;
+
+/**
+ * Bind a function, EventEmitter, or Stream to the provided session object
+ * with an optional context.
+ *
+ * @param  {Function|EventEmitter|Stream} `fn` Object to bind to.
+ * @param  {Object} `session` session-cache object to bind with.
+ * @param  {Object} `context` Optional context to bind to the sesssion.
+ * @return {*} Bound object
+ */
+
+function sessionify (fn, session, context) {
+  if (!session) {
+    throw new Error('sessionify expects a session-cache object as `session`');
+  }
+
+  var bind = session.bind;
+  if (typeof fn === 'object' && fn.on) {
+    bind = session.bindEmitter;
+  }
+
+  if (context) {
+    bind.call(session, fn, context);
+  } else {
+    bind.call(session, fn);
+  }
+  return fn;
+};
